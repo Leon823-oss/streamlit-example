8fd01045a4927f8c686da5418d4560f5b8c6e085
Leon823-oss <62862852+Leon823-oss@users.noreply.github.com>
2022-01-26 17:58:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,211 +1,211 @@
 # _*_ coding:utf-8 _*_
 
 import streamlit as st
-import multiprocessing
-from multiprocessing import Process, Value, Array
-import time, os
-import snap7
-import struct
+# import multiprocessing
+# from multiprocessing import Process, Value, Array
+# import time, os
+# import snap7
+# import struct
 import re
 
 from streamlit_lottie import st_lottie
 import json
 
 
-class S7_Client(object):
-    def __init__(self):
-        self.online = False
-        self.concount = 0
-        self.plc = snap7.client.Client()
-        self.plc.set_connection_type(1)  # 1 for PG, 2 for OP, 3 to 10 for S7 Basic
+# class S7_Client(object):
+#     def __init__(self):
+#         self.online = False
+#         self.concount = 0
+#         self.plc = snap7.client.Client()
+#         self.plc.set_connection_type(1)  # 1 for PG, 2 for OP, 3 to 10 for S7 Basic
 
-    # def connect(self, ip, rack=0, slot=1):
-    #     try:
-    #         # mylog.logger.info("正在尝试连接 PLC %s" % (ip,))
-    #         self.plc.connect(ip, rack, slot)
-    #         if self.plc.get_connected():
-    #             # mylog.log/ger.info(ip + ' 连接成功')
-    #             self.online = True
-    #
-    #     except Exception as e:
-    #         self.plc.connect(ip, rack, slot)
-    #
-    #     finally:
-    #         if not self.plc.get_connected():
-    #             self.plc.connect(ip, rack, slot)
+#     # def connect(self, ip, rack=0, slot=1):
+#     #     try:
+#     #         # mylog.logger.info("正在尝试连接 PLC %s" % (ip,))
+#     #         self.plc.connect(ip, rack, slot)
+#     #         if self.plc.get_connected():
+#     #             # mylog.log/ger.info(ip + ' 连接成功')
+#     #             self.online = True
+#     #
+#     #     except Exception as e:
+#     #         self.plc.connect(ip, rack, slot)
+#     #
+#     #     finally:
+#     #         if not self.plc.get_connected():
+#     #             self.plc.connect(ip, rack, slot)
 
-    def connect(self, ip, rack=0, slot=1):
-        while not self.plc.get_connected():
-           try:
-               self.plc.connect(ip, rack, slot)
-               time.sleep(0.2)
-           except Exception as e:
-               # print("not online")
-               continue
+#     def connect(self, ip, rack=0, slot=1):
+#         while not self.plc.get_connected():
+#            try:
+#                self.plc.connect(ip, rack, slot)
+#                time.sleep(0.2)
+#            except Exception as e:
+#                # print("not online")
+#                continue
 
 
-    def disconnect(self):
-        self.plc.disconnect()
-        # mylog.logger.info('连接断开')
+#     def disconnect(self):
+#         self.plc.disconnect()
+#         # mylog.logger.info('连接断开')
 
-    def get_connected(self):
-        return self.plc.get_connected()
+#     def get_connected(self):
+#         return self.plc.get_connected()
 
-    def get_cpu_state(self):
-        return self.plc.get_cpu_state()
+#     def get_cpu_state(self):
+#         return self.plc.get_cpu_state()
 
-    # region Read 数据
-    def read_db_bool(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 1)
-        db_tuple_bool = struct.unpack("?", db_str)
-        return db_tuple_bool[0]
+#     # region Read 数据
+#     def read_db_bool(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 1)
+#         db_tuple_bool = struct.unpack("?", db_str)
+#         return db_tuple_bool[0]
 
-    def read_i_bool(self, db_num, start):
-        i_str = self.plc.read_area(0x81, db_num, start, 1)
-        i_tuple_bool = struct.unpack("?", i_str)
-        return i_tuple_bool[0]
+#     def read_i_bool(self, db_num, start):
+#         i_str = self.plc.read_area(0x81, db_num, start, 1)
+#         i_tuple_bool = struct.unpack("?", i_str)
+#         return i_tuple_bool[0]
 
-    def read_q_bool(self, db_num, start):
-        q_str = self.plc.read_area(0x82, db_num, start, 1)
-        q_tuple_bool = struct.unpack("?", q_str)
-        return q_tuple_bool[0]
+#     def read_q_bool(self, db_num, start):
+#         q_str = self.plc.read_area(0x82, db_num, start, 1)
+#         q_tuple_bool = struct.unpack("?", q_str)
+#         return q_tuple_bool[0]
 
-    def read_db_byte(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 1)
-        db_tuple_byte = struct.unpack("B", db_str)
-        return db_tuple_byte[0]
+#     def read_db_byte(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 1)
+#         db_tuple_byte = struct.unpack("B", db_str)
+#         return db_tuple_byte[0]
 
-    def read_I_byte(self, db_num, start):
-        db_str = self.plc.read_area(0x81, db_num, start, 1)
-        db_tuple_byte = struct.unpack("B", db_str)
-        return db_tuple_byte[0]
+#     def read_I_byte(self, db_num, start):
+#         db_str = self.plc.read_area(0x81, db_num, start, 1)
+#         db_tuple_byte = struct.unpack("B", db_str)
+#         return db_tuple_byte[0]
 
-    def read_Q_byte(self, db_num, start):
-        db_str = self.plc.read_area(0x82, db_num, start, 1)
-        db_tuple_byte = struct.unpack("B", db_str)
-        return db_tuple_byte[0]
+#     def read_Q_byte(self, db_num, start):
+#         db_str = self.plc.read_area(0x82, db_num, start, 1)
+#         db_tuple_byte = struct.unpack("B", db_str)
+#         return db_tuple_byte[0]
 
-    def read_db_int(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 2)
-        db_tuple_int = struct.unpack(">h", db_str)  # 大小端
-        return db_tuple_int[0]
+#     def read_db_int(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 2)
+#         db_tuple_int = struct.unpack(">h", db_str)  # 大小端
+#         return db_tuple_int[0]
 
-    def read_db_uint(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 2)
-        db_tuple_int = struct.unpack(">H", db_str)  # 大端
-        return db_tuple_int[0]
+#     def read_db_uint(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 2)
+#         db_tuple_int = struct.unpack(">H", db_str)  # 大端
+#         return db_tuple_int[0]
 
-    def read_db_dint(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 4)
-        db_tuple_dint = struct.unpack(">i", db_str)  # 大端
-        return db_tuple_dint[0]
+#     def read_db_dint(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 4)
+#         db_tuple_dint = struct.unpack(">i", db_str)  # 大端
+#         return db_tuple_dint[0]
 
-    def read_db_udint(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 4)
-        db_tuple_dint = struct.unpack(">I", db_str)  # 大端
-        return db_tuple_dint[0]
+#     def read_db_udint(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 4)
+#         db_tuple_dint = struct.unpack(">I", db_str)  # 大端
+#         return db_tuple_dint[0]
 
-    def read_db_real(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 4)
-        db_tuple_real = struct.unpack("!f", db_str)
-        return db_tuple_real[0]
+#     def read_db_real(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 4)
+#         db_tuple_real = struct.unpack("!f", db_str)
+#         return db_tuple_real[0]
 
-    def read_db_char(self, db_num, start):
-        db_str = self.plc.read_area(0x84, db_num, start, 1)
-        db_tuple_char = struct.unpack("c", db_str)
-        return db_tuple_char[0]
+#     def read_db_char(self, db_num, start):
+#         db_str = self.plc.read_area(0x84, db_num, start, 1)
+#         db_tuple_char = struct.unpack("c", db_str)
+#         return db_tuple_char[0]
 
-    def read_db_string(self, db_num, start, length):
-        db_str = self.plc.read_area(0x84, db_num, start, length + 2)
-        db_tuple_str = struct.unpack(str(length + 2) + 's', db_str)
-        return str(db_tuple_str[0][2:], encoding='gbk')
+#     def read_db_string(self, db_num, start, length):
+#         db_str = self.plc.read_area(0x84, db_num, start, length + 2)
+#         db_tuple_str = struct.unpack(str(length + 2) + 's', db_str)
+#         return str(db_tuple_str[0][2:], encoding='gbk')
 
-    def read_M_int(self, db_num, start):
-        db_str = self.plc.read_area(0x83, db_num, start, 2)
-        db_tuple_int = struct.unpack(">h", db_str)
-        return db_tuple_int[0]
+#     def read_M_int(self, db_num, start):
+#         db_str = self.plc.read_area(0x83, db_num, start, 2)
+#         db_tuple_int = struct.unpack(">h", db_str)
+#         return db_tuple_int[0]
 
-    def read_M_bool(self, db_num, start):
-        i_str = self.plc.read_area(0x83, db_num, start, 1)
-        i_tuple_bool = struct.unpack("?", i_str)
-        return i_tuple_bool[0]
+#     def read_M_bool(self, db_num, start):
+#         i_str = self.plc.read_area(0x83, db_num, start, 1)
+#         i_tuple_bool = struct.unpack("?", i_str)
+#         return i_tuple_bool[0]
 
-    def read_M_byte(self, db_num, start):
-        db_str = self.plc.read_area(0x83, db_num, start, 1)
-        db_tuple_byte = struct.unpack("B", db_str)
-        return db_tuple_byte[0]
+#     def read_M_byte(self, db_num, start):
+#         db_str = self.plc.read_area(0x83, db_num, start, 1)
+#         db_tuple_byte = struct.unpack("B", db_str)
+#         return db_tuple_byte[0]
 
-    def read_db(self, db_num, start, size):
-        # db_buf = self.plc.db_read(db_num, start, size)
-        # db_tuple_read = struct.unpack("B", db_buf)
-        return self.plc.db_read(db_num, start, size)
+#     def read_db(self, db_num, start, size):
+#         # db_buf = self.plc.db_read(db_num, start, size)
+#         # db_tuple_read = struct.unpack("B", db_buf)
+#         return self.plc.db_read(db_num, start, size)
 
-    def read_plc_datetime(self, db_num, start):
-        YEAR = self.read_db_uint(db_num, start)
-        MONTH = self.read_db_byte(db_num, start + 2)
-        DAY = self.read_db_byte(db_num, start + 3)
-        WEEKDAY = self.read_db_byte(db_num, start + 4)
-        HOUR = self.read_db_byte(db_num, start + 5)
-        MINUTE = self.read_db_byte(db_num, start + 6)
-        SECOND = self.read_db_byte(db_num, start + 7)
-        NANOSECOND = self.read_db_udint(db_num, start + 8)
-        strdatetime = "%04d" % YEAR + '-' + "%02d" % MONTH + "-" + "%02d" % DAY + " " + "%02d" % HOUR + ':' + \
-                      "%02d" % MINUTE + ':' + "%02d" % SECOND + ':' + "%04d" % NANOSECOND
-        return strdatetime
+#     def read_plc_datetime(self, db_num, start):
+#         YEAR = self.read_db_uint(db_num, start)
+#         MONTH = self.read_db_byte(db_num, start + 2)
+#         DAY = self.read_db_byte(db_num, start + 3)
+#         WEEKDAY = self.read_db_byte(db_num, start + 4)
+#         HOUR = self.read_db_byte(db_num, start + 5)
+#         MINUTE = self.read_db_byte(db_num, start + 6)
+#         SECOND = self.read_db_byte(db_num, start + 7)
+#         NANOSECOND = self.read_db_udint(db_num, start + 8)
+#         strdatetime = "%04d" % YEAR + '-' + "%02d" % MONTH + "-" + "%02d" % DAY + " " + "%02d" % HOUR + ':' + \
+#                       "%02d" % MINUTE + ':' + "%02d" % SECOND + ':' + "%04d" % NANOSECOND
+#         return strdatetime
 
-    # endregion
-    # region Write 数据
-    def write_db_bool(self, db_num, start, data):
-        _data = struct.pack("!?", data)
-        self.plc.write_area(0x84, db_num, start, _data)
+#     # endregion
+#     # region Write 数据
+#     def write_db_bool(self, db_num, start, data):
+#         _data = struct.pack("!?", data)
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_byte(self, db_num, start, data):
-        _data = struct.pack("B", data)
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_byte(self, db_num, start, data):
+#         _data = struct.pack("B", data)
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_int(self, db_num, start, data):
-        _data = struct.pack(">h", data)
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_int(self, db_num, start, data):
+#         _data = struct.pack(">h", data)
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_uint(self, db_num, start, data):
-        _data = struct.pack(">H", data)
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_uint(self, db_num, start, data):
+#         _data = struct.pack(">H", data)
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_dint(self, db_num, start, data):
-        _data = struct.pack(">i", data)
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_dint(self, db_num, start, data):
+#         _data = struct.pack(">i", data)
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_dint(self, db_num, start, data):
-        _data = struct.pack(">I", data)
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_dint(self, db_num, start, data):
+#         _data = struct.pack(">I", data)
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_float(self, db_num, start, data):
-        _data = struct.pack("!f", data)
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_float(self, db_num, start, data):
+#         _data = struct.pack("!f", data)
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_string(self, db_num, start, data):
-        _data = struct.pack(str(len(data) + 2) + 's', data.encode('utf-8'))  #
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_string(self, db_num, start, data):
+#         _data = struct.pack(str(len(data) + 2) + 's', data.encode('utf-8'))  #
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_db_char(self, db_num, start, data):
-        _data = struct.pack("c", data.encode('utf-8'))
-        self.plc.write_area(0x84, db_num, start, _data)
+#     def write_db_char(self, db_num, start, data):
+#         _data = struct.pack("c", data.encode('utf-8'))
+#         self.plc.write_area(0x84, db_num, start, _data)
 
-    def write_M_int(self, db_num, start, data):
-        _data = struct.pack(">h", data)
-        self.plc.write_area(0x83, db_num, start, _data)
+#     def write_M_int(self, db_num, start, data):
+#         _data = struct.pack(">h", data)
+#         self.plc.write_area(0x83, db_num, start, _data)
 
-    def write_M_bool(self, db_num, start, data):
-        _data = struct.pack("!?", data)
-        self.plc.write_area(0x83, db_num, start, _data)
+#     def write_M_bool(self, db_num, start, data):
+#         _data = struct.pack("!?", data)
+#         self.plc.write_area(0x83, db_num, start, _data)
 
-    def write_M_byte(self, db_num, start, data):
-        _data = struct.pack("B", data)
-        self.plc.write_area(0x83, db_num, start, _data)
+#     def write_M_byte(self, db_num, start, data):
+#         _data = struct.pack("B", data)
+#         self.plc.write_area(0x83, db_num, start, _data)
 
-    def write_Q_byte(self, db_num, start, data):
-        _data = struct.pack("B", data)
-        self.plc.write_area(0x82, db_num, start, _data)
+#     def write_Q_byte(self, db_num, start, data):
+#         _data = struct.pack("B", data)
+#         self.plc.write_area(0x82, db_num, start, _data)
     # endregion
 
 
